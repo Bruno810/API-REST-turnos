b9a89674de9e4aa997d1bbef2605c8256bed2332
Bruno Nicolas Masucci <bru.masucci@gmail.com>
2026-05-21 20:33:34 -0300

    Nuevo mensaje cuando las fechas o las horas con incorrectas

--- src/main/java/com/bruno/turnosapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bruno/turnosapi/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.bruno.turnosapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -32,5 +33,11 @@ public class GlobalExceptionHandler{
                 .collect(Collectors.joining(", "));
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(mensaje, HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("El formato de los datos enviados no es válido. Verificá que la hora tenga formato HH:mm:ss y la fecha YYYY-MM-DD", HttpStatus.BAD_REQUEST.value()));
     }
 }
